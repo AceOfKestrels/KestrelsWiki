20e0640d814aa6e5c39710040d08c92607d6f19b
AceOfKestrels <aceofkestrels@gmail.com>
2024-11-21 10:55:38 +0100

    Add icon to sidebar toggle button

--- api/wwwroot/js/script.js
+++ api/wwwroot/js/script.js
@@ -1,7 +1,9 @@
 function toggleSidebarLeft() {
     document.getElementById("layout").classList.toggle("left-collapsed")
+    document.getElementById("toggle-left-button").classList.toggle("turned")
 }
 
 function toggleSidebarRight() {
     document.getElementById("layout").classList.toggle("right-collapsed")
+    document.getElementById("toggle-right-button").classList.toggle("turned")
 }
