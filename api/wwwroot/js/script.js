7f56fa7ecf5bfa5f3ea6b222b9efa9663b7049b1
AceOfKestrels <aceofkestrels@gmail.com>
2024-11-21 09:45:55 +0100

    Add sidebar toggle

---
+++ api/wwwroot/js/script.js
@@ -0,0 +1,7 @@
+function toggleSidebarLeft() {
+    document.getElementById("layout").classList.toggle("left-collapsed")
+}
+
+function toggleSidebarRight() {
+    document.getElementById("layout").classList.toggle("right-collapsed")
+}
